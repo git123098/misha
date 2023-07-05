6de99f9be5772a6933924a2426748d074f7dc9fe
git123098 <108227090+git123098@users.noreply.github.com>
2023-07-05 20:34:59 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "20.04.2004"; // Дата рождения
+var fullname = "Мандра Михайло Павлович" // ФИО
+var name = "Михайло" // Имя
+
+// Паспорт
+var passport_id = "915629895" // Номер паспорта
+
+// КПП
+var kpp_id = "2843376351" // ИНН
+
+// Права
+var rights_valid_until = "06.03.2023" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 7231" // Права "Видав
+var rights_id = "KBE928639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.04.2023" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
